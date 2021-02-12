374ac59da1d32a243890be9258c025d5a42d0971
thaa15 <74519289+thaa15@users.noreply.github.com>
2021-02-12 07:50:01 +0700

    exercise

--- pages/component/ExerciseComponents/ContentExercise.tsx
+++ pages/component/ExerciseComponents/ContentExercise.tsx
@@ -119,7 +119,7 @@ const ContentExercise = () => {
                 textAlign="justify"
                 mb="60px"
                 >
-                    Berikut adalah projek yang pernah dilakukan oleh Exercise<br/>
+                    Berikut adalah projek yang pernah dilakukan oleh Exercise <br/>
                     1. Fintech Automation System <br/>
                     2. Electric Bike & Solar Charging Station <br/>
                     3. Microwave Imaging System <br/>
